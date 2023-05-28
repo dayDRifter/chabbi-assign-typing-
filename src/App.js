6f7661aae2a5003bdb87404d2b5dc26055f66f84
Ujjwal Gupta <guptaujjwal600@gmail.com>
2023-05-28 11:58:57 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import TypingApp from "./components/Typing-App/TypingApp";
+
+function App() {
+  return (
+    <div className="App">
+      <TypingApp />
+    </div>
+  );
+}
+
+export default App;
